d3bb73bc95a4a87971c3be4005c08948809977fd
javascriptninja815 <xandokan815@protonmail.com>
2019-01-18 08:42:40 -0800

    changed input into simple div

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.data !== undefined) {
     var id = JSON.parse(msg.data);
     chrome.runtime.sendMessage({
-      'detected_email': document.getElementById(id).getElementsByTagName('input')[0].value
+      'detected_email': document.getElementById('hidden_email').innerHTML
     });
   }
 });

f3671103d2fee0736f8fa7394cb730a3a608b22e
javascriptninja815 <xandokan815@protonmail.com>
2019-01-18 08:07:06 -0800

    detect email from hidden input

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
-chrome.runtime.sendMessage({
-  'title': document.title
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  if (msg.data !== undefined) {
+    var id = JSON.parse(msg.data);
+    chrome.runtime.sendMessage({
+      'detected_email': document.getElementById(id).getElementsByTagName('input')[0].value
+    });
+  }
 });
+
--- popup.js
+++ popup.js
@@ -1,8 +1,20 @@
 function onPageDetailsReceived(pageDetails)  { 
-  document.getElementById('title').value = pageDetails.title; 
+  document.getElementById('detected_email').value = pageDetails.detected_email;
 }
-window.addEventListener('load', function(evt) {
+function detect_hidden_email() {
+  var id = document.getElementById('detect_for').value;
+  chrome.tabs.query({
+      active: true,
+      currentWindow: true
+  }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+          data: JSON.stringify(id)
+      });
+  });
   chrome.runtime.getBackgroundPage(function(eventPage) {
     eventPage.getPageDetails(onPageDetailsReceived);
   });
+}
+window.addEventListener('load', function(evt) {
+  document.getElementById('detect_for').addEventListener('change', detect_hidden_email);
 })

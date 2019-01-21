93ef4e245be8ed2a5838ff91ab1acc73b0391926
javascriptninja815 <xandokan815@protonmail.com>
2019-01-21 10:35:37 -0800

    send email as only message

--- popup.js
+++ popup.js
@@ -15,6 +15,30 @@ function detect_hidden_email() {
     eventPage.getPageDetails(onPageDetailsReceived);
   });
 }
+function send_email(){
+
+  event.preventDefault();
+
+  var host = document.getElementById('host').value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  var detected_email = document.getElementById('detected_email').value;
+  var sender = document.getElementById('sender').value;
+  var subject = document.getElementById('subject').value;
+  
+    Email.send({
+      Host : host,
+      Username : username,
+      Password : password,
+      To : detected_email,
+      From : sender,
+      Subject : subject,
+      Body : "This is test message "
+    }).then(
+      message => alert(message)
+    );
+}
 window.addEventListener('load', function(evt) {
   document.getElementById('detect_for').addEventListener('change', detect_hidden_email);
+  document.getElementById('send_email').addEventListener('submit', send_email);
 })
